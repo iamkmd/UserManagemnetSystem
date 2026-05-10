3b6863737e084b22b5672fdb84847833cd63badb
Mansi Kabade <124434933+iamkmd@users.noreply.github.com>
2026-05-10 18:18:57 +0530

    AppContextListener.java

--- src/main/java/controller/AppContextListener.java
+++ src/main/java/controller/AppContextListener.java
@@ -22,25 +22,14 @@ public class AppContextListener  implements ServletContextListener {
 			 ServletContext ctx = sce.getServletContext();
 			 
 			 //read the System.prperties file as stream
-			 
 			 InputStream in = ctx.getResourceAsStream("/WEB-INF/system.properties");
 			 
 			 //load into properties object
 			 Properties props = new Properties();
 			 props.load(in);
-			 
-			 //store in ServletContext so everyone can use it
-			 
-
-		        System.out.println("driver   = " + props.getProperty("driver"));
-		        System.out.println("url      = " + props.getProperty("url"));
-		        System.out.println("username = " + props.getProperty("username"));
-		        System.out.println("password = " + props.getProperty("password"));
-
-		        // ← store whole props object in one go
-		        ctx.setAttribute("props", props);
-			 
-			 
+			
+		     //store whole props object in one go
+		     ctx.setAttribute("props", props);
 			 logger.info("system.properties loaded succesfully");
 		 }
 		 catch(Exception e)
